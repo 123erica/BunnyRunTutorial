8ed334de6f2e0a8950b5895bc8c142d547753a35
123erica <41613225+123erica@users.noreply.github.com>
2018-07-30 13:11:18 +1200

    Bunny Run Changes

---
+++ Assets/Scripts/bunnyjump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunnyjump : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody2d;
+
+    public float bunnyjumpforce = 500f; 
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp("Jump"))
+        {
+            myRigidBody2d.AddForce(transform.up * bunnyjumpforce);
+        }
+	}
+}
